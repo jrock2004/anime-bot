0615459e72ae17f79200ed67d99b0f51e6675e1f
John Costanzo <jrock2004@gmail.com>
2020-04-03 19:55:08 -0400

    Fixing build by adding canvas support

--- webpack.functions.js
+++ webpack.functions.js
@@ -3,4 +3,8 @@ const Dotenv = require('dotenv-webpack');
 // @see https://github.com/netlify/netlify-lambda#webpack-configuration
 module.exports = {
   plugins: [new Dotenv()],
+  externals: ['canvas'],
+  node: {
+    canvas: 'empty',
+  },
 };
